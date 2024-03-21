48ce8844968d0cd3d5480602f0b9999cb685308b
Nguyen Minh Tam <beosex11223@gmail.com>
2024-03-22 01:23:42 +0700

    viet them truy van

--- database/solution_b.sql
+++ database/solution_b.sql
@@ -1,3 +1,8 @@
+1--Liệt kê các hóa đơn của khách hàng, thông tin hiển thị gồm: mã user, tên user, mã
+hóa đơn
+SELECT u.user_id, u.user_name, o.order_id
+FROM users u
+JOIN orders o ON u.user_id = o.user_id;
 
 2--Liệt kê số lượng các hóa đơn của khách hàng: mã user, tên user, số đơn hàng
 SELECT u.user_id, u.user_name, COUNT(o.order_id) AS so_don_hang
@@ -8,6 +13,15 @@ Câu 3: SELECT orders.order_id, COUNT(order_details.order_detail_id) AS number_o
 FROM orders
 JOIN order_details ON orders.order_id = order_details.order_id
 GROUP BY orders.order_id;
+4--Liệt kê thông tin mua hàng của người dùng: mã user, tên user, mã đơn hàng, tên sản
+phẩm. Lưu ý: gôm nhóm theo đơn hàng, tránh hiển thị xen kẻ các đơn hàng với nhau
+SELECT u.user_id, u.user_name, o.order_id, p.product_name
+FROM users u
+JOIN orders o ON u.user_id = o.user_id
+JOIN order_details od ON o.order_id = od.order_id
+JOIN products p ON od.product_id = p.product_id
+GROUP BY o.order_id;
+
 5--Liệt kê 7 người dùng có số lượng đơn hàng nhiều nhất, thông tin hiển thị gồm: mã
 user, tên user, số lượng đơn hàng
 SELECT users.id AS user_id, users.name AS user_name, COUNT(orders.id) AS order_count
